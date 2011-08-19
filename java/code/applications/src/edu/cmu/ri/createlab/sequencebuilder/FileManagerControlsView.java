363d64512d20706f36059235ef9026f3aee8aebd
jenncross99@gmail.com <jenncross99@gmail.com@2fe31110-8f58-cb13-be32-c5f50f879a7a>
2011-08-19 17:55:59 +0000

    Some additional "focus" indicators added to LnF. For meeting demo.

--- java/code/applications/src/edu/cmu/ri/createlab/sequencebuilder/FileManagerControlsView.java
+++ java/code/applications/src/edu/cmu/ri/createlab/sequencebuilder/FileManagerControlsView.java
@@ -3,6 +3,7 @@ package edu.cmu.ri.createlab.sequencebuilder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -77,6 +78,8 @@ final class FileManagerControlsView
       this.programElementListCellRenderer = programElementListCellRenderer;
 
       deleteButton.setIcon(ImageUtils.createImageIcon("/edu/cmu/ri/createlab/sequencebuilder/images/deleteMark.png"));
+      deleteButton.setMnemonic(KeyEvent.VK_D);
+      appendButton.setMnemonic(KeyEvent.VK_A);
 
       panel.setLayout(new GridBagLayout());
       panel.setBackground(Color.WHITE);
