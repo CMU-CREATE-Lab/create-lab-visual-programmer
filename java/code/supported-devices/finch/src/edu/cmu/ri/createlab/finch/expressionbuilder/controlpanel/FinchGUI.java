e6775fd3283ec4fbd76f14498448adc4f79e5294
jenncross99@gmail.com <jenncross99@gmail.com@2fe31110-8f58-cb13-be32-c5f50f879a7a>
2012-01-22 05:53:51 +0000

    More modifications towards a finch interface in expression builder.

--- java/code/supported-devices/finch/src/edu/cmu/ri/createlab/finch/expressionbuilder/controlpanel/FinchGUI.java
+++ java/code/supported-devices/finch/src/edu/cmu/ri/createlab/finch/expressionbuilder/controlpanel/FinchGUI.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 import edu.cmu.ri.createlab.expressionbuilder.controlpanel.DeviceGUI;
 import edu.cmu.ri.createlab.expressionbuilder.controlpanel.ServiceControlPanel;
 import edu.cmu.ri.createlab.terk.services.accelerometer.AccelerometerService;
+import edu.cmu.ri.createlab.terk.services.audio.AudioService;
 import edu.cmu.ri.createlab.terk.services.buzzer.BuzzerService;
 import edu.cmu.ri.createlab.terk.services.led.FullColorLEDService;
 import edu.cmu.ri.createlab.terk.services.obstacle.SimpleObstacleDetectorService;
@@ -34,49 +35,75 @@ public final class FinchGUI extends DeviceGUI
       final Component spacer = SwingUtils.createRigidSpacer();
       final JPanel gui = createFinchGUI(serviceControlPanelMap, serviceDeviceToggleButtonMap);
       final JPanel guiControlPanels = createControlPanelsGUI(serviceControlPanelMap);
+      final JPanel audioControlPanel = createAudioPanel(serviceControlPanelMap, serviceDeviceToggleButtonMap);
 
-
+      final Component buzzerControlPanel = serviceControlPanelMap.get(BuzzerService.TYPE_ID).getComponent();
+          
      //TODO: mainPanel is now a GridBag Layout, create appropriate GridBagConstraints
           final GridBagConstraints c = new GridBagConstraints();
-          //Center Area Layout
 
-          c.fill = GridBagConstraints.HORIZONTAL;
-          c.gridwidth = 1;
+          c.fill = GridBagConstraints.NONE;
+          c.gridwidth = 2;
           c.gridheight = 1;
           c.gridx = 0;
           c.gridy = 0;
-          c.weighty = 1.0;
+          c.weighty = .5;
+          c.weightx = 1.0;
           c.anchor = GridBagConstraints.CENTER;
-          c.insets = new Insets(0, 0, 0, 0);
+          c.insets = new Insets(5, 5, 5, 5);
+          mainPanel.add(audioControlPanel, c);
+
+
+          c.fill = GridBagConstraints.NONE;
+          c.gridwidth = 1;
+          c.gridheight = 1;
+          c.gridx = 1;
+          c.gridy = 1;
+          c.weighty = .5;
+          c.weightx = .5;
+          c.anchor = GridBagConstraints.LINE_START;
+          c.insets = new Insets(5, 10, 5, 10);
           mainPanel.add(gui, c);
 
-          c.fill = GridBagConstraints.HORIZONTAL;
+          c.fill = GridBagConstraints.NONE;
           c.gridwidth = 1;
           c.gridheight = 1;
           c.gridx = 0;
           c.gridy = 1;
-          c.weighty = 1.0;
-          c.anchor = GridBagConstraints.CENTER;
-          c.insets = new Insets(0, 0, 0, 0);
+          c.weighty = .5;
+          c.weightx = .5;
+          c.anchor = GridBagConstraints.LINE_END;
+          c.insets = new Insets(5, 10, 5, 10);
           mainPanel.add(guiControlPanels, c);
+
+          c.fill = GridBagConstraints.NONE;
+          c.gridwidth = 2;
+          c.gridheight = 1;
+          c.gridx = 0;
+          c.gridy = 2;
+          c.weighty = .5;
+          c.weightx = .5;
+          c.anchor = GridBagConstraints.CENTER;
+          c.insets = new Insets(5, 5, 5, 5);
+          mainPanel.add(buzzerControlPanel, c);
 
       }
 
    private JPanel createFinchGUI(final Map<String, ServiceControlPanel> serviceControlPanelMap, final Map<String, SortedMap<Integer, JCheckBox>> serviceDeviceToggleButtonMap)
       {
-      final JPanel orbsPanel = createHorizontalButtonPanel(serviceControlPanelMap.get(FullColorLEDService.TYPE_ID),
-              serviceDeviceToggleButtonMap.get(FullColorLEDService.TYPE_ID),
-              false,
-              BACKGROUND_COLOR, BOX_COLOR, false,
-              "image.green");
+          final JPanel orbsPanel = createHorizontalButtonPanel(serviceControlPanelMap.get(FullColorLEDService.TYPE_ID),
+                  serviceDeviceToggleButtonMap.get(FullColorLEDService.TYPE_ID),
+                  true,
+                  BACKGROUND_COLOR, BOX_COLOR, true,
+                  "image.white");
 
       final JPanel buzzerPanel = createHorizontalButtonPanel(serviceControlPanelMap.get(BuzzerService.TYPE_ID),
                                                            serviceDeviceToggleButtonMap.get(BuzzerService.TYPE_ID),
-                                                           false,
-                                                           BACKGROUND_COLOR, BOX_COLOR, false,
-                                                         "image.green");
+                                                            true,
+                                                           BACKGROUND_COLOR, BOX_COLOR, true,
+                                                           "image.white");
       final JPanel motorsPanel = new JPanel();
-      motorsPanel.add(SwingUtils.createLabel("Open Loop Motors Panel will go here"));
+      motorsPanel.add(SwingUtils.createLabel("Open Loop Motors Panel"));
       /*
       TODO: add this
       final JPanel motorsPanel = createHorizontalButtonPanel(serviceControlPanelMap.get(OpenLoopVelocityControllableMotorService.TYPE_ID),
@@ -87,7 +114,7 @@ public final class FinchGUI extends DeviceGUI
       final JLayeredPane layers = new JLayeredPane();
 
       final JPanel panel = new JPanel();
-      Dimension board_size = new Dimension(300, 400);
+      Dimension board_size = new Dimension(400, 300);
 
 
       panel.setLayout(new GridBagLayout());
@@ -98,19 +125,19 @@ public final class FinchGUI extends DeviceGUI
 
           c.fill = GridBagConstraints.NONE;
           c.gridwidth = 1;
-          c.gridheight = 1;
+          c.gridheight = 3;
           c.gridx = 0;
           c.gridy = 0;
           c.weighty = 1.0;
-          c.anchor = GridBagConstraints.CENTER;
+          c.anchor = GridBagConstraints.LINE_START;
           c.insets = new Insets(0, 0, 0, 0);
           panel.add(orbsPanel, c);
 
           c.fill = GridBagConstraints.NONE;
           c.gridwidth = 1;
           c.gridheight = 1;
-          c.gridx = 0;
-          c.gridy = 1;
+          c.gridx = 1;
+          c.gridy = 0;
           c.weighty = 1.0;
           c.anchor = GridBagConstraints.CENTER;
           c.insets = new Insets(0, 0, 0, 0);
@@ -119,7 +146,7 @@ public final class FinchGUI extends DeviceGUI
           c.fill = GridBagConstraints.NONE;
           c.gridwidth = 1;
           c.gridheight = 1;
-          c.gridx = 0;
+          c.gridx = 1;
           c.gridy = 2;
           c.weighty = 1.0;
           c.anchor = GridBagConstraints.CENTER;
@@ -135,6 +162,7 @@ public final class FinchGUI extends DeviceGUI
 
           panel.setBounds(0, 0, board_size.width, board_size.height);
           finch_image.setBounds(0, 0, board_size.width, board_size.height);
+          finch_image.setName("purpleElement");
           layers.add(panel, new Integer(1));
           layers.add(finch_image, new Integer(0));
 
@@ -160,11 +188,51 @@ public final class FinchGUI extends DeviceGUI
       final JPanel panel = new JPanel();
       panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
       panel.add(serviceControlPanelMap.get(FullColorLEDService.TYPE_ID).getComponent());
-      panel.add(serviceControlPanelMap.get(BuzzerService.TYPE_ID).getComponent());
+      
       //TODO: panel.add(serviceControlPanelMap.get(OpenLoopVelocityControllableMotorService.TYPE_ID).getComponent());
 
       return panel;
       }
+
+       private JPanel createAudioPanel(final Map<String, ServiceControlPanel> serviceControlPanelMap, final Map<String, SortedMap<Integer, JCheckBox>> serviceDeviceToggleButtonMap)
+       {
+           final Component audio = serviceControlPanelMap.get(AudioService.TYPE_ID).getComponent();
+
+           final JPanel buttonPanel = new JPanel(new SpringLayout());
+           buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+           final ServiceControlPanel serviceControlPanel = serviceControlPanelMap.get(AudioService.TYPE_ID);
+           final SortedMap<Integer, JCheckBox> checkBoxMap = serviceDeviceToggleButtonMap.get(AudioService.TYPE_ID);
+           for (final int deviceId : checkBoxMap.keySet())
+           {
+               final JCheckBox checkBox = checkBoxMap.get(deviceId);
+               buttonPanel.add(checkBox);
+           }
+           final JLabel label = SwingUtils.createLabel(serviceControlPanel.getShortDisplayName());
+
+           final JPanel audioPanel = new JPanel();
+           audioPanel.setLayout(new BoxLayout(audioPanel, BoxLayout.Y_AXIS));
+           audioPanel.add(label);
+           audioPanel.add(buttonPanel);
+           audioPanel.setName("finchAudioButtons");
+           buttonPanel.setName("finchAudioButtons");
+
+           final JPanel audioSpeakerButton = new JPanel();
+
+           audioSpeakerButton.setLayout(new BoxLayout(audioSpeakerButton, BoxLayout.X_AXIS));
+           audioSpeakerButton.setName("speaker_panel_finch");
+
+           audioSpeakerButton.add(audioPanel);
+
+           final JPanel mainPanel = new JPanel();
+           mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+           mainPanel.add(audioSpeakerButton);
+           mainPanel.add(SwingUtils.createRigidSpacer(10));
+           mainPanel.add(audio);
+           mainPanel.setName("audio_holder");
+           // mainPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+           return mainPanel;
+       }
+
 
    public void setStageTitleField(JTextField textfield)
       {
