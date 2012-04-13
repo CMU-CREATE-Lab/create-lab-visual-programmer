50aa6a8b38dc523e365f2f25e50bd4d5aa075838
bartley@cmu.edu <bartley@cmu.edu@2fe31110-8f58-cb13-be32-c5f50f879a7a>
2012-04-13 19:06:07 +0000

    Don't need these anymore.

--- java/expression-builder.bat
+++
@@ -1,1 +0,0 @@
-java -Djna.library.path=./code/applications/dist -Djava.library.path=./code/applications/dist -cp .\code\applications\dist\visual-programmer-applications.jar edu.cmu.ri.createlab.expressionbuilder.ExpressionBuilder
