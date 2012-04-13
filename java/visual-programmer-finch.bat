e45f15421a016208d9a8739258ab66bf51d4769b
bartley@cmu.edu <bartley@cmu.edu@2fe31110-8f58-cb13-be32-c5f50f879a7a>
2012-04-13 19:08:39 +0000

    Can now specify which VisualProgrammerDevice impl class to use with a system property.  E.g., use -DVisualProgrammerDevice.class=edu.cmu.ri.createlab.finch.visualprogrammer.FinchVisualProgrammerDevice to launch for Finch.  Defaults to hummingbird if not specified (since there is one and only one create-lab-visual-programmer.properties file in the source, and it specifies the HummingbirdVisualProgrammerDevice).  Fixes issue 131

---
+++ java/visual-programmer-finch.bat
@@ -0,0 +1,1 @@
+java -DVisualProgrammerDevice.class=edu.cmu.ri.createlab.finch.visualprogrammer.FinchVisualProgrammerDevice -Djna.library.path=./code/applications/dist -Djava.library.path=./code/applications/dist -cp ./code/applications/dist/visual-programmer-applications.jar edu.cmu.ri.createlab.visualprogrammer.VisualProgrammer
