e9f03e225ffd1201e43e3eb96bf380abd6feb896
bartley@cmu.edu <bartley@cmu.edu@2fe31110-8f58-cb13-be32-c5f50f879a7a>
2013-04-17 15:53:32 +0000

    Set the application name for Mac OS X (now shows "Visual Programmer" in the menu bar)

--- trunk/java/visual-programmer-finch.sh
+++ trunk/java/visual-programmer-finch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -DVisualProgrammerDevice.class=edu.cmu.ri.createlab.finch.visualprogrammer.FinchVisualProgrammerDevice -Djna.library.path=./code/applications/dist -Djava.library.path=./code/applications/dist -cp ./code/applications/dist/visual-programmer-applications.jar edu.cmu.ri.createlab.visualprogrammer.VisualProgrammer;
+java -Xdock:name="Visual Programmer" -DVisualProgrammerDevice.class=edu.cmu.ri.createlab.finch.visualprogrammer.FinchVisualProgrammerDevice -Djna.library.path=./code/applications/dist -Djava.library.path=./code/applications/dist -cp ./code/applications/dist/visual-programmer-applications.jar edu.cmu.ri.createlab.visualprogrammer.VisualProgrammer;
